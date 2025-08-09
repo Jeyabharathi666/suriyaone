9f5db648a8e37ea2899f1de0c06f259a8947b76c
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2025-08-09 11:39:58 +0530

    Update suriyaonep3.py

--- suriyaonep3.py
+++ suriyaonep3.py
@@ -19,10 +19,12 @@ URL = ["https://chartink.com/screener/copy-copy-copy-sreelakshmi-guruvayoorappan
        "https://chartink.com/screener/copy-atp-above-long-cash-2",
        "https://chartink.com/screener/copy-copy-future-and-options-2-1-4",
        "https://chartink.com/screener/copy-atp-above-long-fut1",
-       "https://chartink.com/screener/copy-copy-daily-min-f-0-trade-2"]
+       "https://chartink.com/screener/copy-copy-daily-min-f-0-trade-2",
+       "https://chartink.com/screener/copy-atr-volume-f-o-200-wkly-rsi-70-new",
+       "https://chartink.com/screener/copy-bollinger-band-trick-909"]
        
 sheet_id = "18uM89Cjv6_DZmAbLXyNUFciVrGUBizbQLsrRZhNIDK0"
-worksheet_name = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16","p17","p18"]
+worksheet_name = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16","p17","p18","p19","p20"]
 
 def scrape_chartink(URL, worksheet_name):
     print(f"🚀 Starting Chartink scrape for {worksheet_name}...")
