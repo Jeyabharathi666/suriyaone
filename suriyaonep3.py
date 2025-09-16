1480ef8f15e1284bc4b9684d9ef8c1f183631d6f
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2025-09-16 13:49:27 +0530

    Update suriyaonep3.py

--- suriyaonep3.py
+++ suriyaonep3.py
@@ -119,8 +119,8 @@ def scrape_chartink(url, worksheet_name):
                 print(f"⚠️ No records found at {url}. Skipping update.")
                 rows = []
             else:
-                page.wait_for_selector("table.w-full tbody tr", timeout=60000)
-                table_rows = page.query_selector_all("table.w-full tbody tr")
+                page.wait_for_selector("div.relative table tbody tr", timeout=60000)
+                table_rows = page.query_selector_all("div.relative table tbody tr")
                 print(f"📥 Extracted {len(table_rows)} rows.")
 
                 rows = []
