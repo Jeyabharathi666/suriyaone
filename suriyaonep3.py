da6328e32f3813a2215acdb363a4164bcb046190
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2026-04-10 15:24:20 +0530

    Update suriyaonep3.py

--- suriyaonep3.py
+++ suriyaonep3.py
@@ -4,33 +4,28 @@ import google_sheets
 import time
 
 URLS = [
-     "https://chartink.com/screener/copy-copy-copy-sreelakshmi-guruvayoorappan-b-atr-volume-rocket-8",
-    "https://chartink.com/screener/agp-bullish2-p5",
-    "https://chartink.com/screener/aaa13-vp-sheshapathi",
-    "https://chartink.com/screener/agp-shesha-bulloong1",
-    "https://chartink.com/screener/shesha-magic-buy-love",
-    "https://chartink.com/screener/copy-mahi-2-master-trader-vishnu-final-40-address-this-urgent-bellinaire-38-to-47-3",
-    "https://chartink.com/screener/agp-bullish2",
-    "https://chartink.com/screener/copy-atp-above-long-24",
-    "https://chartink.com/screener/22-nw-shesha-magic-buy-love-fut",
-    "https://chartink.com/screener/copy-nr-f-0",
-    "https://chartink.com/screener/copy-rk-position-f-0",
-    "https://chartink.com/screener/copy-f-0-future",
-    "https://chartink.com/screener/smbg2-new-multibegger-stocks-for-next-few-days",
-    "https://chartink.com/screener/cash-tss-momentum-long",
-    "https://chartink.com/screener/copy-atp-above-long-cash-2",
-    "https://chartink.com/screener/copy-copy-future-and-options-2-1-4",
-    "https://chartink.com/screener/copy-atp-above-long-fut1",
+     "https://chartink.com/screener/50-sreelakshmi-guruvayoorappan-b-atr-volume-rocket",
+     "https://chartink.com/screener/50agp-bullish2-p5",
+     "https://chartink.com/screener/50aaa13-vp-sheshapathi",
+     "https://chartink.com/screener/50-oneeeeeee",
+     "https://chartink.com/screener/50shesha-magic-buy-love",
+     "https://chartink.com/screener/50atp-above-long",
+     "https://chartink.com/screener/50-daily-min-f-0-trade",
+     "https://chartink.com/screener/50-the-best-btst",
+     "https://chartink.com/screener/50-22-nw-shesha-magic-buy-love",
+     "https://chartink.com/screener/50-bearish-engulifing-see-after-3-15-pm-for-next-day-trade",
+     "https://chartink.com/screener/50-bearish-maribozu",
+     "https://chartink.com/screener/50aaaagp-shesha-bearish-2",
+     "https://chartink.com/screener/50stocks-in-downtrend",
+     "https://chartink.com/screener/copy-perfect-bearish-3537",
+     "https://chartink.com/screener/copy-chanakya-bearish-scanner-working-2803",
     "https://chartink.com/screener/copy-copy-daily-min-f-0-trade-2",
     "https://chartink.com/screener/copy-atr-volume-f-o-200-wkly-rsi-70-new",
     "https://chartink.com/screener/copy-the-best-btst-193",
     "https://chartink.com/screener/22-nw-shesha-magic-buy-love",
     "https://chartink.com/screener/copy-richie-rich-f-0-2",
-    "https://chartink.com/screener/all-u1-nk-sir-s-uptrend-stocks-all-time-uptrend",
-    "https://chartink.com/screener/copy-sjbl6ch-shesha-buy-bollinger-band-weekly",
-    "https://chartink.com/screener/copy-copy-bb-blaster-2",
-    "https://chartink.com/screener/copy-atr-volume-f-o-200-wkly-rsi-70-16",
-    "https://chartink.com/screener/copy-multibagar-5" ]
+    "https://chartink.com/screener/all-u1-nk-sir-s-uptrend-stocks-all-time-uptrend"
+     ]
        
 sheet_id = "18uM89Cjv6_DZmAbLXyNUFciVrGUBizbQLsrRZhNIDK0"
 worksheet_names = [
